9505888a64662a094ba6bd00a7c3d5264c154f83
liberaliscomputing <liberalis.computing@gmail.com>
2016-09-02 12:27:46 -0400

    add mouse events and visualization transition

--- js/network-mapping.js
+++ js/network-mapping.js
@@ -1,5 +1,6 @@
-//initialize a base graph
-
+// initialize a base graph
+/**
+ * define default style
 var defStylesheet = cytoscape.stylesheet()
 	.selector('node')
 		.style({
@@ -20,6 +21,7 @@ var defStylesheet = cytoscape.stylesheet()
 			'curve-style': 'unbundled-bezier'
 		})
 ;
+ */
 
 var altStylesheet = cytoscape.stylesheet()
 	.selector('node')
@@ -30,11 +32,11 @@ var altStylesheet = cytoscape.stylesheet()
 			'font-size': 10,
 			'background-fit': 'contain',
 			'background-color': '#666',
-			'border-width': 2,
+			'border-width': 1,
 			'border-opacity': 1,
 			'border-color': '#666',
 			'text-opacity': 0,
-			'text-valign': 'center',
+			'text-valign': 'top',
 			'text-halign': 'right',
 			'text-background-color': '#666',
 			'text-background-opacity': 1,
@@ -42,7 +44,8 @@ var altStylesheet = cytoscape.stylesheet()
 			'text-border-opacity': 1,
 			'text-border-color': '#666',
 			'text-border-width': 3,
-			'color': '#fff'
+			'color': '#fff',
+			'overlay-color': '#666'
 		})
 	.selector('node[img]')
 		.style({
@@ -50,15 +53,18 @@ var altStylesheet = cytoscape.stylesheet()
 		})
 	.selector('node.hover')
 		.style({
-			'text-opacity': 1
+			'text-opacity': 1,
+			'width': 40,
+			'height': 40,
+			'font-size': 20
 		})
 	.selector('edge')
 		.style({
 			'line-color': '#666',
-			'width': 10,
-			'opacity': 0.25,
-			'curve-style': 'haystack',
-			'haystack-radius': 0
+			'width': 5,
+			'opacity': 0.5,
+			'curve-style': 'unbundled-bezier',
+			'overlay-color': '#666'
 		})
 ;
 
@@ -68,14 +74,28 @@ var cy = cytoscape({
 		nodes: dataset.nodes,
 		edges: dataset.links
 	},
-	style: defStylesheet,
-	layout: { name: 'concentric' }
+	style: altStylesheet,
+	layout: {
+		name: 'concentric',
+		animate: true,
+		animationDuration: 2000
+	}
 });
 
 //open a clicked node's Twitter profile in a new window tab
 var setEvents = cy
 //Open a clicked handle's Twitter profile
-	.on('click', 'node', function() {
+	.on('click', 'node', function(){
+		cy.animate({
+			zoom: 1.0,
+			center: {
+				eles: this
+			}
+		}, {
+			duration: 1000
+		});
+	})
+	.on('doubleTap', 'node', function(){
 		var url = 'https://twitter.com/';
 		window.open(url + this.id(),'_blank');
 	})
@@ -86,3 +106,54 @@ var setEvents = cy
 		this.removeClass('hover');
 	})
 ;
+
+// add a custom doubleTab event
+var tappedBefore;
+var tappedTimeout;
+cy.on('tap', function(event) {
+	var tappedNow = event.cyTarget;
+	if (tappedTimeout && tappedBefore) {
+		clearTimeout(tappedTimeout);
+	}
+	if(tappedBefore === tappedNow) {
+		tappedNow.trigger('doubleTap');
+		tappedBefore = null;
+	} else {
+		tappedTimeout = setTimeout(function(){ tappedBefore = null; }, 300);
+		tappedBefore = tappedNow;
+	}
+});
+
+// set layout change events
+$('#concentric').on('mousedown', function(){
+	cy.layout({
+		name: 'concentric',
+		animate: true,
+		animationDuration: 2000
+	});
+});
+
+$('#cose-bilkent').on('mousedown', function(){
+	cy.layout({
+		name: 'cose-bilkent',
+		tile: false,
+		animate: true,
+		animationDuration: 2000
+	});
+});
+
+$('#circle').on('mousedown', function(){
+	cy.layout({
+		name: 'circle',
+		animate: true,
+		animationDuration: 2000
+	});
+});
+
+$('#grid').on('mousedown', function(){
+	cy.layout({
+		name: 'grid',
+		animate: true,
+		animationDuration: 2000
+	});
+});
