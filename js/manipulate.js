45fb0ac561f393fa548142356755d3121e63c64f
liberaliscomputing <liberalis.computing@gmail.com>
2016-09-08 11:16:23 -0400

    initialize flip()

--- js/manipulate.js
+++ js/manipulate.js
@@ -2,10 +2,15 @@
  * Created by openhtml on 9/1/16.
  */
 // set the height of the visualisation as responsive
-var h = window.innerHeight * 0.95;
+//var w = window.innerWidth;
+var h = window.innerHeight;
 $("#vis").css("height", h);
 
-// initializing tooltip
+// initialize flip
+$("#card").flip();
+$("#card").css({ "height": h, "background": "#fff" });
+
+// initialize tooltip
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
 })
