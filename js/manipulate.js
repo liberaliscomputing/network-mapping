8249aed44567593892cfdc1a0f19cd5b04b5eac1
liberaliscomputing <liberalis.computing@gmail.com>
2016-09-15 16:31:53 -0400

    initialize scrollspy

--- js/manipulate.js
+++ js/manipulate.js
@@ -2,9 +2,9 @@
  * Created by openhtml on 9/1/16.
  */
 // set the height of the visualisation as responsive
-//var w = window.innerWidth;
+var w = window.innerWidth;
 var h = window.innerHeight;
-$("#vis").css("height", h);
+$("#vis").css({ "height": h, "width": w });
 
 // initialize flip
 $("#card").flip();
@@ -14,3 +14,19 @@ $("#card").css({ "height": h, "background": "#fff" });
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
 })
+
+// initialize scrollspy
+$(document).ready(function(){
+	$('body').scrollspy({target: ".navbar", offset: 50});
+	$("#navbar a").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+});
