3091bdb4068ea97853d12c8715fd0ff5a15284f3
liberaliscomputing <liberalis.computing@gmail.com>
2016-09-16 03:30:35 -0400

    deprecate flip, tooltip, and scrollspy

--- js/manipulate.js
+++ js/manipulate.js
@@ -4,12 +4,14 @@
 // set the height of the visualisation as responsive
 var w = window.innerWidth;
 var h = window.innerHeight;
+
 $("#vis").css({ "height": h, "width": w });
 
 // initialize flip
-$("#card").flip();
-$("#card").css({ "height": h, "background": "#fff" });
+//$("#leaders").flip();
+//$(".card").css({ "height": h, "background": "#fff" });
 
+/*
 // initialize tooltip
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
@@ -30,3 +32,4 @@ $(document).ready(function(){
 		}
 	});
 });
+*/
