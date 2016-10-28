4ba3d0adcef5bab2d6689bf61f7ded7c0098fae2
liberaliscomputing <liberalis.computing@gmail.com>
2016-10-28 15:16:24 +0100

    modularize register-alert

---
+++ js/register-alert.js
@@ -0,0 +1,9 @@
+// register a given handle
+$(function () {
+  $("#register").click(function () {
+    var handle = document.getElementById("handle").value;
+    // check
+    alert("Thank you! We registered your twitter handle to the app! " +
+      "It takes some time to get it rendered on mapping.");
+  });
+});
